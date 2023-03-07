8d3af3cb1bf18f8736667d085b71c7c89ac28c35
Melody Forest <mforest@gsu.edu>
2023-03-07 14:11:19 -0500

    Refactored index.php

--- public/index.php
+++ public/index.php
@@ -2,33 +2,40 @@
 
 declare(strict_types=1);
 
-use DI\ContainerBuilder  as DIContainerBuilder;
-use GAState\Web\Slim\App as SlimApp;
-use GAState\Web\Slim\Env as SlimEnv;
+use DI\ContainerBuilder   as DIContainerBuilder;
+use GAState\MySlimApp\Env as Env;
+use GAState\Web\Slim\App  as BaseApp;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 ob_start();
 
-SlimEnv::load(
-    $_ENV['BASE_DIR'] ?? __DIR__ . '/../',
-    $_ENV['BASE_URI'] ?? dirname($_SERVER['SCRIPT_NAME'])
-);
+(function () {
+    Env::load(
+        strval(realpath(Env::getString(Env::BASE_DIR, __DIR__ . '/../'))),
+        Env::getString(Env::BASE_URI, dirname(dirname($_SERVER['SCRIPT_NAME'])))
+    );
 
-$cmplDir = SlimEnv::getString(SlimEnv::DI_CMPL_DIR);
-$proxyDir = SlimEnv::getString(SlimEnv::DI_PRXY_DIR);
-$defFile = SlimEnv::getString(SlimEnv::DI_DEF_FILE);
+    $containerBuilder = (new DIContainerBuilder())
+        ->useAttributes(true)
+        ->addDefinitions(Env::getString(Env::DI_DEF_FILE));
 
-$container = (new DIContainerBuilder())
-    // ->enableCompilation($cmplDir)
-    // ->writeProxiesToFile(true, $proxyDir)
-    ->useAttributes(true)
-    ->addDefinitions($defFile)
-    ->build();
+    if (Env::getBool(Env::DI_ENABLE_CMPL)) {
+        $containerBuilder = $containerBuilder
+            ->enableCompilation(Env::getString(Env::DI_CMPL_DIR))
+            ->writeProxiesToFile(true, Env::getString(Env::DI_PRXY_DIR));
+    }
 
-$app = $container->get(SlimApp::class);
-if ($app instanceof SlimApp) {
+    $app = $containerBuilder
+        ->build()
+        ->get(BaseApp::class);
+
+    // This should never happen but... let's handle anyway
+    if (!$app instanceof BaseApp) {
+        throw new RuntimeException("\$app is not an instance of " . BaseApp::class);
+    }
+
     $app->run();
-}
+})();
 
 ob_end_flush();
